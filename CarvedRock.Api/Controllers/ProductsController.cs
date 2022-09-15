54af46977ef5ed32f67d326811cb10391d37b49a
Ivan MSI <arigatory@gmail.com>
2022-09-16 02:59:55 +0300

    3-feat: add serilog

--- CarvedRock.Api/Controllers/ProductsController.cs
+++ CarvedRock.Api/Controllers/ProductsController.cs
@@ -2,27 +2,25 @@
 using CarvedRock.Api.ApiModels;
 using CarvedRock.Api.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace CarvedRock.Api.Controllers
 {
-    [ApiController]
+  [ApiController]
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly ILogger<ProductsController> _logger;
         private readonly IProductLogic _productLogic;
 
-        public ProductsController(ILogger<ProductsController> logger, IProductLogic productLogic)
+        public ProductsController(IProductLogic productLogic)
         {
-            _logger = logger;
             _productLogic = productLogic;
         }
 
         [HttpGet]
         public IEnumerable<Product> GetProducts(string category = "all")
         {
-            _logger.LogInformation("Starting controller action GetProducts for {category}", category);
+            Log.Information("Starting controller action GetProducts for {category}", category);
 
             return _productLogic.GetProductsForCategory(category);
         }
