5452a1a7f23f4a33430c30d47c1a94ff1dfae75a
Ivan MSI <arigatory@gmail.com>
2022-09-16 05:14:09 +0300

    4-feat: add seq

--- CarvedRock.Api/Startup.cs
+++ CarvedRock.Api/Startup.cs
@@ -10,42 +10,44 @@ using CarvedRock.Api.Middleware;
 
 namespace CarvedRock.Api
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<IProductLogic, ProductLogic>();
-            services.AddScoped<IQuickOrderLogic, QuickOrderLogic>();
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CarvedRock.Api", Version = "v1" });
-            });
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseMiddleware<CustomExceptionHandlingMiddleware>();
-            if (env.IsDevelopment())
-            {                
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CarvedRock.Api v1"));
-            }
-
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddScoped<IProductLogic, ProductLogic>();
+      services.AddScoped<IQuickOrderLogic, QuickOrderLogic>();
+
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "CarvedRock.Api", Version = "v1" });
+      });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseMiddleware<CustomExceptionHandlingMiddleware>();
+      if (env.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CarvedRock.Api v1"));
+      }
+
+      app.UseCustomRequestLogging();
+
+      app.UseRouting();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
